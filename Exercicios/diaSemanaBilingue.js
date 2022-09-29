9f277228d4d769c0b01760b25b0595052116a85d
ClaudioSts <claudiosantos.arb@gmail.com>
2022-09-29 11:08:24 +0100

    29sep

--- Exercicios/diaSemanaBilingue.js
+++ Exercicios/diaSemanaBilingue.js
@@ -46,3 +46,80 @@ function diaDaSemanaPorExtensoBilingue(data, lingua) {
             }
         }
     }
+
+
+////////////////////////////////////////////////////////////////
+
+function mesPorExtensoBilingue(data, lingua) {
+    console.log(data, lingua)
+        // escreve aqui a função
+    
+        let mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    
+    
+        let m = data.getMonth()
+    
+        if (lingua == "pt") {
+            switch (m) {
+                case 0:
+                    return mes[m]
+                case 1:
+                    return mes[m]
+                case 2:
+                    return mes[m]
+                case 3:
+                    return mes[m]
+                case 4:
+                    return mes[m]
+                case 5:
+                    return mes[m]
+                case 6:
+                    return mes[m]
+                case 7:
+                    return mes[m]
+                case 8:
+                    return mes[m]
+                case 9:
+                    return mes[m]
+                case 10:
+                    return mes[m]
+                case 11:
+                    return mes[m]
+                case 12:
+                    return mes[m]
+                    
+            }
+    
+        } else if (lingua == "en") {
+            switch (m) {
+                case 0:
+                    return month[m]
+                case 1:
+                    return month[m]
+                case 2:
+                    return month[m]
+                case 3:
+                    return month[m]
+                case 4:
+                    return month[m]
+                case 5:
+                    return month[m]
+                case 6:
+                    return month[m]
+                case 7:
+                    return month[m]
+                case 8:
+                    return month[m]
+                case 9:
+                    return month[m]
+                case 10:
+                    return month[m]
+                case 11:
+                    return month[m]
+                case 12:
+                    return month[m]
+    
+            }
+        }
+    }
